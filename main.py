8cffa17bbebd4200de980892aa52faf11d17cbb5
val500 <varun.valada@gmail.com>
2020-11-21 20:39:28 -0500

    Added actual music playing

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
+import sys
+import musicalbeeps
+from read_music import scan_image
+import cv2
+from  matplotlib import pyplot  as plt
+
 def main():
+    args = sys.argv[1:]
+    if len(args) != 1:
+        print("Too many args!")
+        return
     
+    for arg in args:
+        filename = arg
+    
+    notes = scan_image(filename)
+    print(notes)
+    img = cv2.imread(filename)
+    plt.imshow(img)
+    plt.show()
+    player = musicalbeeps.Player(volume = 0.3, mute_output = False)
+    note_map = {'d1': 'D', 'e1': 'E', 'f1': 'F', 'g1': 'G', 'a1': 'A', 'b1': 'B', 'c1': 'C', 'd2': 'D5', 'e2': 'E5', 'f2': 'F5', 'g2': 'G5'}
+    
+    for note in notes:
+        player.play_note(note_map[note])
+    
+main()
--- read_music.py
+++ read_music.py
@@ -4,8 +4,8 @@ import math
 from  matplotlib import pyplot  as plt
 from sklearn.cluster import KMeans
 
-def scan_image():
-    imgName = 'music.png'
+def scan_image(imgName):
+    #imgName = 'music.png'
     img = cv2.imread(imgName)
     lines = find_lines(imgName)
     circles = find_circles(imgName)
@@ -24,17 +24,18 @@ def find_lines(imgName):
 
     lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]))
     lines = lines.reshape((lines.shape[0], lines.shape[2]))
-    for line in lines.reshape((3180, 4)):
+    for line in lines:
         pt1 = (line[0], line[1])
         pt2 = (line[2], line[3])
         cv2.line(img, pt1, pt2, (0,0,255), 3)
         line_pts.append((pt1, pt2))
+    plt.imshow(img)
     hor_vals = []
     for pt1, pt2 in line_pts:
         if pt1[1] == pt2[1]:
             hor_vals.append(pt1[1])
     hor_vals = np.array(hor_vals)
-    model = KMeans(5).fit(hor_vals.reshape((-1, 1)))
+    model = KMeans(5, max_iter=300).fit(hor_vals.reshape((-1, 1)))
     return model.cluster_centers_.reshape(5)
     
 def find_circles(imgName):
@@ -47,6 +48,7 @@ def find_circles(imgName):
 
     detector = cv2.SimpleBlobDetector_create(params)
     keypoints = detector.detect(gray)
+    img = cv2.drawKeypoints(img, keypoints, img)
     pts = cv2.KeyPoint_convert(keypoints)
     sort_pts = pts[pts[:,0].argsort()]
     return sort_pts[:,1].reshape(-1)
@@ -62,6 +64,7 @@ def find_notes(notes, lines):
     all_line_pos.append(lines[-1])
     all_line_pos.append(lines[-1] + (lines[-1] - ((lines[-2] + lines[-1]) / 2)))
     all_line_pos = np.array(all_line_pos)
+    print(all_line_pos)
     note_arr = []
     for n in notes:
         note_arr.append(note_vals[np.argmin(np.abs(n - all_line_pos))])
